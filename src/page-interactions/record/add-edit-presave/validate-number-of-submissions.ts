697385451cbb37d32e10acfd3ec21788b6a1a7fa
hari <hari@test.com>
2021-02-07 11:59:28 +0530

    correction

--- src/page-interactions/record/add-edit-presave/validate-number-of-submissions.ts
+++ src/page-interactions/record/add-edit-presave/validate-number-of-submissions.ts
@@ -12,31 +12,43 @@ const interaction: PageInteraction = {
             init: () => {
                 const jobOrderId = form.jobOrder.id;
 
-                var max = myClient.getMaxSubmissions(jobOrderId);
-                var num = myClient.queryNumberOfSubmissions(jobOrderId);
+                //var max = myClient.getMaxSubmissions(jobOrderId);
+                //var num = myClient.queryNumberOfSubmissions(jobOrderId);
 
-                if(num < max)
-                {
-                    form.valid =  true;
-                }
+                return myClient.getMaxSubmissions(jobOrderId).then(response => {
+                    const max = response.data.data.customText9;
+                    
+                    return myClient.queryNumberOfSubmissions(jobOrderId).then(response => {
+                      const num = response.data.total; 
+                      
+                      if(num < max)
+                      {
+                          form.valid =  true;
+                      }
+      
+                      else {
+                          form.valid = false;
+                          form.errorMessage = 'We have reached the maximum number of submissions that the Facility allows.';
+                      }
+                        
+                        return form;
+                    });
+                })
 
-                else {
-                    form.valid = false;
-                    form.errorMessage = 'We have reached the maximum number of submissions that the Facility allows.';
-                }
+                
                 
             },
 
             
             getMaxSubmissions: (jobOrderId) => {
-                var maxSub = API.appBridge.httpGET('/entity/JobOrder/?clientCorporation(customText9)')
+                var maxSub = API.appBridge.httpGET('/entity/JobOrder/${jobOrderId}?fields=clientCorporation(customText9)')
                 return maxSub;
             },
 
             queryNumberOfSubmissions: (jobOrderId) => {
                 const where = 'jobOrder.id = ${jobOrderId}';
 
-                var numSub = API.appBridge.httpGET('/query/JobSubmissions?where=${where}&fields=id')
+                var numSub = API.appBridge.httpGET('/query/JobSubmission?where=${where}&fields=id&showTotalMatched=true')
 
                 return numSub;
             }
